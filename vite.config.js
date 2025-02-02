43477e036ed760afaf60e3225dbf15350c054212
Moheshwar Amarnath Biswas <fluentmoheshwar@outlook.com>
2025-02-02 12:30:32 +0600

    Add PWA support with vite-plugin-pwa and update configuration

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
-import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [tailwindcss()],
+  plugins: [
+    tailwindcss(),
+    VitePWA({
+      manifest: {
+        theme_color: "#FC427B",
+      },
+    }),
+  ],
 });
